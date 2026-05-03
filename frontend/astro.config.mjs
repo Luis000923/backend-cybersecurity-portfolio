48bb5bcae9f11fbe9e4ca398dcfc5ffceb6bde80
Luis hack_version <luis@example.com>
2026-05-02 20:55:32 -0600

    fix(deploy): fix 0 pages build and missing output directory on Hostinger

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: '../dist',
-  publicDir: '../public',
-  trailingSlash: 'always',
+  // Usamos los valores por defecto (dist y public locales)
+  // para que Hostinger no se confunda
   vite: {
     optimizeDeps: {
       exclude: ['esbuild']
